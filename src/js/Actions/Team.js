713479afd4ba33cca4a84e9ded3a8c603a610a60
Daria <n-d-p@mail.ru>
2023-12-14 00:12:13 +0300

    It works

---
+++ src/js/Actions/Team.js
@@ -0,0 +1,32 @@
+export default class Team {
+    constructor() {
+        this.members = new Set();
+    }
+
+    add(character) {
+        if (this.members.has(character)) {
+            throw new Error('Вы не можете добавить персонажа дважды');
+        } else {
+            this.members.add(character);
+        }
+    }
+
+    addAll(...arrMembers) {
+        for (const member of arrMembers) {
+            this.members.add(member);
+        }
+    }
+
+    toArray() {
+        const arrMembers = [];
+        this.members.forEach((member) => arrMembers.push(member));
+        return arrMembers.sort();
+    }
+
+    *[Symbol.iterator]() {
+        const members = this.toArray();
+        for(let member of members){
+            yield member
+        }
+    }
+}
